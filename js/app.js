c6f011a5dfaa6fcb4e80198c6c16fcbc2d32d0ae
Cynthia Clinton <cclinc2005@yahoo.com>
2018-07-27 00:56:42 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,48 +1,71 @@
+
+
+
+/*jshint esversion: 6 */
 //player and enemy inherits from parent construction function
-class Entity {
- constructor () {
+//(function(global) {
+'use Strict';
+
+/**
+ * @desc Superclass holds common functionalities
+ * of Player{} and Enemy{} 
+ */
+class Character {
+  constructor () {
     this.xaxis = 101;
     this.yaxis = 83;
     this.beginX = this.xaxis * 2;
     this.beginY = (this.yaxis * 4) + 55;
     this.sprite =  "images/";
   }
-//render player and enemy on the screen
+//draw player and enemy on the screen
 render() {
    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 }
 
-// Enemies our player must avoid: enemy constructor function
-class Enemy extends Entity {
+/**
+ * Enemies our player must avoid
+ *
+ * @param {int} x - x coord position
+ * @param {int} y - y coord position
+ * @param {int} speed - movement speed
+ */
+class Enemy extends Character {
   constructor(x, y, speed)  {
-  //super constructor alter's parent's class
     super();
     this.x = x;
+    // y === 0 plus offset 55 pixels
     this.y = y + 55;
     this.speed = speed;
     this.sprite += "enemy-bug.png";
   }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
- update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+/**
+ * updates Enemy position 
+ *
+ * @param {int} dt - DeltaTime - a time
+ * delta between ticks
+ */
+update(dt) {
     if (this.x < 505) {
-    this.x += this.speed * dt;
+      this.x += this.speed * dt;
     }
     else {
-   this.x = -101;
+      this.x = -101;
    }
   }
 }
 
-//create player constructor function
-class Player  extends Entity{
+/**
+ * Player character
+ *
+ * @param {int} x - x coord position
+ * @param {int} y - y coord position
+ * @param {string} sprite - Player sprite
+ */
+class Player extends Character {
   constructor () {
-    //super constructor alter's parent's class
     super();
     this.x = this.beginX;
     this.y = this.beginY;
@@ -50,40 +73,50 @@ class Player  extends Entity{
     this.sprite += "char-princess-girl.png";
   }
 
-//when enemy is close enough to player's x and y locations collide move 
-// player back to start position & checksIfGameOver and render modal
+/**
+ * when enemy is close enough to
+ * player's x/y location collide, move
+ * player back to start position,
+ * checksIfGameOver and render modal
+ */
   update() {
     for (let enemy of allEnemies) {
-      if (enemy.x + (this.xaxis/2) > player.x &&
-      enemy.x < player.x + (this.xaxis/2) && (enemy.y === player.y)) {
+      if (enemy.x + 83 > player.x &&
+      enemy.x < player.x + 83 && (enemy.y === player.y)) {
       player.resetPlayer();
-      }
-    }
+     }
+   }
     //check if player reached river
-   if (player.y === 55) {
+    if (player.y === 55) {
        player.wins = true;
     }
   }
- //handleInput() method: creates Player's movement
-  handleInput(keypress) {
+
+/**
+ * Player's movement
+ *
+ * @param {string} key - String of directions that add * movement to
+ */
+ handleInput(keypress) {
     switch(keypress) {
       case 'left':
-        this.x = this.x > 0 ? this.x - this.xaxis : this.x;
+        this.x = this.x > 0 ? this.x - 101 : this.x;
         break;
       case 'up':
-        this.y = this.y > 0 ? this.y - this.yaxis : this.y;
+        this.y = this.y > 0 ? this.y - 83: this.y;
         break;
       case 'right':
-        this.x = this.x < 4 * this.xaxis ? this.x + this.xaxis : this.x;
+        this.x = this.x < 4 * 101 ? this.x + 101: this.x;
         break;
       case 'down':
-        this.y = this.y < 4 * this.yaxis ? this.y + this.yaxis : this.y;
+        this.y = this.y < 4 * 83 ? this.y + 83 : this.y;
         break;
       default:
         break;
-      }
+    }
   }
- //reset player back to starting position on x and y axis
+
+//resets player back to starting coords
   resetPlayer() {
     this.x = this.beginX;
     this.y = this.beginY;
@@ -93,20 +126,23 @@ class Player  extends Entity{
 //Create player instance
 const player = new Player();
 
-// all enemy objects is placed in an array called allEnemies
-const allEnemies = [];
-
 //Create instances of all enemies
-// Enemy parameters
-const enemy1 =new Enemy(-101, 0, 400);
+const enemy1 = new Enemy(-101, 0, 400);
 const enemy2 = new Enemy(-101, 83, 400);
 const enemy3 = new Enemy(-252.5, 83, 400);
-const enemy4 = new Enemy(-303, 175, 300);
-allEnemies.push(enemy1, enemy2, enemy3, enemy4);
+const enemy4 = new Enemy(-303, 166, 375);
+
+
+//all enemy objects is placed in an array called allEnemies
+const allEnemies = [enemy1, enemy2, enemy3, enemy4];
 console.log(allEnemies);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/**
+ * This listens for key presses
+ * sends the keys to your
+ *  Player.handleInput() method.
+ * You don't need to modify this.
+ */
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -115,4 +151,7 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
     player.handleInput(allowedKeys[e.keyCode]);
-});
+})
+
+
+
